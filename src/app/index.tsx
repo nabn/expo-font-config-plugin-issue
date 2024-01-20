5dda7362ad5b24bb241dc526489b6af39980bce6
nabn <nabeen.khadka@gmail.com>
2024-01-20 12:44:30 +1100

    Setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -20,24 +20,21 @@ function Content() {
         <View className="container px-4 md:px-6">
           <View className="flex flex-col items-center gap-4 text-center">
             <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
+              style={{
+                fontFamily: "DMSerifDisplay_400Regular",
+                fontSize: 30,
+              }}
             >
-              Welcome to Project ACME
+              DM Serif Display
             </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on amce. Explore our services now.
+            <Text
+              style={{
+                fontFamily: "DMSerifDisplay_400Regular_Italic",
+                fontSize: 30,
+              }}
+            >
+              DM Serif Display Italic
             </Text>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="#"
-              >
-                Explore
-              </Link>
-            </View>
           </View>
         </View>
       </View>
